76aa08f38c925426e63db2e803777810d0b280cb
aciernoantonio <aciernoantonio.int@gmail.com>
2022-06-10 13:24:45 +0200

    add nav-links section

---
+++ laravel-comics/resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DC Comics</title>
+
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- Styles -->
+        <style>
+            
+        </style>
+
+    </head>
+    <body>
+
+        <header class="site_header">
+            @include('partials.header')
+        </header>
+        <!-- /.site_header -->
+
+        <main class="site_main">
+            @include('partials.main')
+        </main>
+        <!-- /.site_main -->
+        
+        <footer class="site_footer">
+
+            @include('partials.footer')
+
+        <script src="{{asset('js/app.js')}}"></script>
+
+        </footer>
+        <!-- /.site_footer -->
+        
+    </body>
+</html>
---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,51 @@
+<div class="top-banner">
+
+    <div class="container">
+
+        <div class="row justify-content-end text-end py-1 text-white">
+
+            <div class="col-2">DC POWER&#8480; VISA&#9415;</div>
+            <div class="col-2 ps-3">ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i></div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<nav>
+
+    <div class="container py-3">
+
+        <div class="row align-items-center justify-content-between">
+
+            <div class="col">
+
+                <img class="logo" src="{{asset('img/dc-logo.png')}}" alt="">
+
+            </div>
+
+            <div class="col d-flex align-items-center nav-links">
+
+                <a class="pe-4" href="">CHARACTERS</a>
+                <a class="pe-4" href="">COMICS</a>
+                <a class="pe-4" href="">MOVIES</a>
+                <a class="pe-4" href="">TV</a>
+                <a class="pe-4" href="">GAMES</a>
+                <a class="pe-4" href="">COLLECTIBLES</a>
+                <a class="pe-4" href="">VIDEOS</a>
+                <a class="pe-4" href="">FANS</a>
+                <a class="pe-4" href="">NEWS</a>
+                <a href="">SHOP <i class="fa-solid fa-caret-down"></a>
+
+            </div>
+
+            <div class="col"></div>
+
+        </div>
+
+        
+
+    </div>
+
+</nav>
