4f5330978848ff24364fe6eb5bd9ccad99e10db7
aciernoantonio <aciernoantonio.int@gmail.com>
2022-06-16 16:17:12 +0200

    add laravel-comics first part

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,110 @@
+
+<div class="footer-links">
+
+   <div class="container">
+
+        <div class="row justify-content-between align-items-center">
+
+            <div class="col-6 py-5">
+
+                <div class="row">
+
+                    <div class="col d-flex flex-column">
+
+                        <h3 class="mb-3">DC COMICS</h3>
+
+                        <a href="">Characters</a>
+                        <a href="">Comics</a>
+                        <a href="">Movies</a>
+                        <a href="">Tv</a>
+                        <a href="">Games</a>
+                        <a href="">Videos</a>
+                        <a href="">News</a>
+
+                        <h3 class="my-3">SHOP</h3>
+
+                        <a href="">Shop DC</a>
+                        <a href="">Shop DC Collectibles</a>
+                        
+                    </div>
+
+                    <div class="col d-flex flex-column">
+
+                        <h3 class="mb-3">DC</h3>
+
+                        <a href="">Terms Of Use</a>
+                        <a href="">Privacy policy (New)</a>
+                        <a href="">Ad Choices</a>
+                        <a href="">Advertising</a>
+                        <a href="">Jobs</a>
+                        <a href="">Subscriptions</a>
+                        <a href="">Talent Workshops</a>
+                        <a href="">CPSC Certifications</a>
+                        <a href="">Ratings</a>
+                        <a href="">Shop Help</a>
+                        <a href="">Contact Us</a>
+
+                    </div>
+
+                    <div class="col d-flex flex-column">
+
+                        <h3 class="mb-3">SITES</h3>
+
+                        <a href="">DC</a>
+                        <a href="">MAD MAGAZINE</a>
+                        <a href="">DC Kids</a>
+                        <a href="">DC Universe</a>
+                        <a href="">DC Power Visa</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col-6">
+
+                <img class="w-75" src="{{asset('img/dc-logo-bg.png')}}" alt="">
+
+            </div>
+
+            <div class="col-6"></div>
+
+        </div>
+
+    </div>
+</div> 
+
+<div class="footer-bottom">
+
+    <div class="container py-4">
+
+        <div class="row justify-content-between align-items-center">
+
+            <div class="col">
+
+                <a class="footer-btn" href="">SING-IN NOW!</a>
+
+            </div>
+
+            <div class="col d-flex justify-content-center align-items-center">
+
+                <div class="text-primary me-4">
+
+                    FOLLOW US
+
+                </div>
+
+                <img class="me-3" src="{{asset('img/footer-facebook.png')}}" alt="">
+                <img class="me-3" src="{{asset('img/footer-twitter.png')}}" alt="">
+                <img class="me-3" src="{{asset('img/footer-youtube.png')}}" alt="">
+                <img class="me-3" src="{{asset('img/footer-periscope.png')}}" alt="">
+                <img class="me-3" src="{{asset('img/footer-pinterest.png')}}" alt="">
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
